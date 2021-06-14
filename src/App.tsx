0582c9a026d1ace63c8185978ffa9a8d8d1b3233
drifterz13 <mixmyxxe@gmail.com>
2021-06-14 13:43:49 +0700

    Add UI for inserting graph details

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import './App.css'
+
+import React, { useState } from 'react'
+import GraphCreator from './GraphCreator.react'
+
+import type { GraphNode, Link } from './types'
 
 import VisualGraph from './VisualGraph.react'
 
 export default function App() {
+  const [nodes, setNodes] = useState<GraphNode[]>([])
+  const [links, setLinks] = useState<Link[]>([])
+
+  const createGraph = (source: string, target: string, value: number) => {
+    const nodeSet = new Set(nodes.map((node) => node.id))
+    if (!nodeSet.has(source)) {
+      setNodes((prevState) => [...prevState, { id: source }])
+    }
+
+    if (!nodeSet.has(target)) {
+      setNodes((prevState) => [...prevState, { id: target }])
+    }
+
+    // TODO: Ensure graph is directed.
+    setLinks((prevState) => [...prevState, { source, target, value }])
+  }
+
+  console.log('debug', nodes, links)
+
   return (
     <div>
-      <VisualGraph />
+      <GraphCreator createGraph={createGraph} />
+      <VisualGraph nodes={nodes} links={links} />
     </div>
   )
 }
---
+++ src/GraphCreator.react.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+type Props = {
+  createGraph: (source: string, target: string, weight: number) => void
+}
+
+export default function GraphCreator(props: Props) {
+  const [linkWeight, setLinkWeigth] = useState(0)
+  const [sourceNode, setSourceNode] = useState('')
+  const [targetNode, setTargetNode] = useState('')
+
+  const reset = () => {
+    setLinkWeigth(0)
+    setSourceNode('')
+    setTargetNode('')
+  }
+
+  return (
+    <section className="graph-creator">
+      <label>
+        Source node:
+        <input
+          type="text"
+          placeholder="Enter source node"
+          value={sourceNode}
+          onChange={(e) => setSourceNode(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Target node:
+        <input
+          type="text"
+          placeholder="Enter source node"
+          value={targetNode}
+          onChange={(e) => setTargetNode(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Link weight:
+        <input
+          type="number"
+          placeholder="Enter link weight (1-100)"
+          min={1}
+          max={100}
+          value={linkWeight}
+          onChange={(e) => setLinkWeigth(+e.target.value)}
+        />
+      </label>
+
+      <button
+        type="button"
+        onClick={() => {
+          props.createGraph(sourceNode, targetNode, linkWeight)
+          reset()
+        }}
+      >
+        Create
+      </button>
+    </section>
+  )
+}
